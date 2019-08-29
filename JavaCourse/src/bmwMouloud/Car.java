21efae7fd7a0b141c0cb6f8be5fbdfbed9ca4add
mouloudwissam <achfrimouloud@gmail.com>
2019-08-28 23:21:09 -0400

    car,bmw

---
+++ JavaCourse/src/bmwMouloud/Car.java
@@ -0,0 +1,14 @@
+package bmwMouloud;
+
+public class Car {
+	public int model;
+	public float price;
+	public short max_speed;
+	public int Sum(int a,int b) {
+		int x =Sum(12,13);
+		int y =Sum(13,14);
+		System.out.print(x);
+		System.out.print(x*y);
+		return a*b;
+	}
+}
